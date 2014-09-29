22c270e32c9c0c0f78a0977e23a6e6a598dbb53f
Brian Youngblood <brian@brianyoungblood.com>
2014-09-29 18:41:45 -0500

    Update clickwhilenext.sh
    save path and all pages zip file changed to all_pages.zip

--- clickwhilenext.sh
+++ clickwhilenext.sh
@@ -11,18 +11,18 @@ for app in "${apps[@]}"
 do
 echo "---- Starting application/$app -----"
 
-zipfile="$savepath/$app/$app.zip"
+zipfile="$savepath/$app/generated_pdfs/all_pages.zip"
 
 if [[ -f $zipfile ]]; then
     echo "File $zipfile found - skip!"
-        cd $savepath/$app
+        cd $savepath/$app/generated_pdfs
         ls -lah .
         cd $scripthome
 else
 echo "File $zipfile not found."
 casperjs clickwhilenext.js --user=$username --password=$password --siteurl="$siteurl" --appid=$app --savepath="$savepath"
-        cd $savepath/$app
-        zip $app.zip *
+        cd $savepath/$app/generated_pdfs
+        zip all_pages.zip *
         ls -lah .
         cd $scripthome
 
